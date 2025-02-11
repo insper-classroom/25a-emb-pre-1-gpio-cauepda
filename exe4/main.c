2afdc8de34f3f6d594274edc937eeec82f62e155
cauepda <cauepda@al.insper.edu.br>
2025-02-11 19:28:26 -0300

    4 ex

--- exe4/main.c
+++ exe4/main.c
@@ -2,10 +2,45 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
+const int led_R = 5;
+const int led_P = 8;
+const int led_B = 11;
+const int led_Y = 15;
+const int BTN = 28;
+
 int main() {
   stdio_init_all();
 
+  gpio_init(led_R);
+  gpio_init(led_P);
+  gpio_init(led_B);
+  gpio_init(led_Y);
+  gpio_init(BTN);
+
+  gpio_set_dir(led_R, GPIO_OUT);
+  gpio_set_dir(led_P, GPIO_OUT);
+  gpio_set_dir(led_B, GPIO_OUT);
+  gpio_set_dir(led_Y, GPIO_OUT);
+  gpio_set_dir(BTN, GPIO_IN);
+
+  gpio_pull_up(BTN);
+
   while (true) {
-    // Use delay de 300 ms entre os estados!
+    if (!gpio_get(BTN)) {
+      gpio_put(led_R, 1);
+      sleep_ms(300);
+      gpio_put(led_R, 0);
+      gpio_put(led_P, 1);
+      sleep_ms(300);
+      gpio_put(led_P, 0);
+      gpio_put(led_B, 1);
+      sleep_ms(300);
+      gpio_put(led_B, 0);
+      gpio_put(led_Y, 1);
+      sleep_ms(300);
+      gpio_put(led_Y, 0);
+      while (!gpio_get(BTN)) {};
+    }
+
   }
 }
