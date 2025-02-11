2d4a426d7e0636dfd622948f74b3d2bcec27d0b7
cauepda <cauepda@al.insper.edu.br>
2025-02-11 19:10:01 -0300

    scnd ex

--- exe2/main.c
+++ exe2/main.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 
 const int BTN_PIN = 26;
+const int BTN_PIN_B = 7;
 
 int main() {
   stdio_init_all();
@@ -10,12 +11,21 @@ int main() {
   gpio_init(BTN_PIN);
   gpio_set_dir(BTN_PIN, GPIO_IN);
   gpio_pull_up(BTN_PIN);
+  
+  gpio_init(BTN_PIN_B);
+  gpio_set_dir(BTN_PIN_B, GPIO_IN);
+  gpio_pull_up(BTN_PIN_B);
+
 
   while (true) {
     if (!gpio_get(BTN_PIN)) {
       printf("Botao 1\n");
       while (!gpio_get(BTN_PIN)) {
       };
-    }
+    } else if (!gpio_get(BTN_PIN_B)) {
+      printf("Botão 2\n");
+      while (!gpio_get(BTN_PIN_B)) {
+      };
+      }
   }
 }
